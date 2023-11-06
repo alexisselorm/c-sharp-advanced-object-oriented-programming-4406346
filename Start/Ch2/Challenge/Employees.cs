fc5b01237597739c4dc555b8ea5716c9789bcbf6
Alexis Selorm Gbeckor-Kove <54552023+alexisselorm@users.noreply.github.com>
2023-11-06 10:37:50 +0000

    Challenge complete

--- Start/Ch2/Challenge/Employees.cs
+++ Start/Ch2/Challenge/Employees.cs
@@ -4,9 +4,49 @@
 public class Employee {
     public Employee() {}
 
+    private readonly int _id;
     // YOUR CODE GOES HERE
+    public Employee(int Id, string fullName, string department){
+        ID=Id;
+        FullName=fullName;
+        Department=department;
+    }
+
+    public required int ID{
+        get;
+        init;
+    }
+    public required string Department {get;set;}
+
+    public string FullName {get;set;}
+
+    public virtual void AdjustPay(decimal percentage){
+
+    }
+
+
 
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
 }
 
 // DEFINE OTHER CLASSES HERE
+public class HourlyEmployee : Employee {
+    
+    public HourlyEmployee(){}
+    public decimal PayRate {get;set;}
+    public override void AdjustPay(decimal percentage)
+    {
+        PayRate += PayRate * percentage;
+    }
+}
+
+public class SalariedEmployee : Employee {
+    
+    public SalariedEmployee(){}
+    public decimal Salary {get;set;}
+
+    public override void AdjustPay(decimal percentage)
+    {
+        Salary+= Salary * percentage;
+    }
+}
