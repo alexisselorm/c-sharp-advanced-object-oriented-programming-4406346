beeca2820e7fcf846239a1b892d3b4ce87e09128
Alexis Selorm Gbeckor-Kove <54552023+alexisselorm@users.noreply.github.com>
2023-11-06 09:16:39 +0000

    Object initialization

--- Start/Ch1/AnonTypes/Program.cs
+++ Start/Ch1/AnonTypes/Program.cs
@@ -1,13 +1,33 @@
 ﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Creating and using Anonymous types
+var myobj = new {
+    Name="Alexis",
+    Street = "123 Main Street",
+    Address= new {
+        Country="Ghana",
+        Part="West Africa"
+    }
+};
 
 // TODO: Anonymous types can be declared using "new" and { }, similar to JS
 
 
+
 // Anonymous types are read-only and cannot be modified
+// myobj.Name="Alex";
 // myobj.Name = "Jane Doe"; # will cause an error
 
+Console.WriteLine(myobj);
+Console.WriteLine(myobj.Name);
+Console.WriteLine(myobj.Address.Country);
+
 // TODO: To change a value, use non-destructive mutation and "with" clause
+var myobj2 = myobj with {Name="Fefe",Street="saa"};
+
+Console.WriteLine(myobj2.Name);
+myobj = myobj2;
+Console.WriteLine(myobj);
+Console.WriteLine(myobj.GetType().GetProperty("Job"));
 
 
 // TODO: You can check to see if an anonymous object contains a property
--- Start/Ch2/ObjectInitializers/Program.cs
+++ Start/Ch2/ObjectInitializers/Program.cs
@@ -2,13 +2,27 @@
 // Using object and collection initializers
 
 // TODO: Use the initializer syntax to create new objects
-
-
+Dog dog = new Dog {Name="Fido",Age=4,IsTrained=true};
+Cat cat = new Cat {Name="MeowingTon", Age=7,IsDeclawed=false};
+Console.WriteLine($"Dog: {dog.Name}");
+Console.WriteLine($"Cat: {cat.Name}");
 // TODO: Initializers can be used on anonymous types
-
+var pet = new {Name="Pet"};
+Console.WriteLine(pet.Name);
 
 // TODO: Collections can also be initialized this way
-
+int[] numbers = new int[]{1,2,3,4,5,6};
+Console.WriteLine(numbers.Length);
 
 // TODO: Initialize a collection with a set of objects
-
+PetOwner owner = new PetOwner{
+    Name="Alexis",
+    Pets = new List<Pet>{
+        new Dog{ Name="July",Age=5},
+        new Cat{ Name="PussInBoots",Age=12}
+    }
+};
+Console.WriteLine(owner.Name);
+foreach(Pet pets in  owner.Pets){
+Console.WriteLine(pets.Name);
+}
