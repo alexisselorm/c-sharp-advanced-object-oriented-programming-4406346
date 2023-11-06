fdc2e1d34ca4225702f4883be6b1c7eb13370541
Alexis Selorm Gbeckor-Kove <54552023+alexisselorm@users.noreply.github.com>
2023-11-06 09:59:47 +0000

    Required properties

--- Start/Ch2/RequiredProps/Program.cs
+++ Start/Ch2/RequiredProps/Program.cs
@@ -1,14 +1,17 @@
 ﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // The "required" modifier
 
+
 // Construct some objects using Object Initializer syntax
 Employee emp1 = new Employee() {FirstName="John", LastName="Doe", ID=1, Department="Sales"};
 Employee emp2 = new Employee() {FirstName="Jane", LastName="Deaux", ID=2, Department="R&D"};
 Employee emp3 = new Employee() {FirstName="Jim", LastName="Dough", ID=3, Department="Marketing"};
 
 // TODO: Construct an object using the constructor
-
+Employee emp4 = new Employee("Joe","Dah",4,"Marketing");
 
 Console.WriteLine(emp1);
 Console.WriteLine(emp2);
 Console.WriteLine(emp3);
+Console.WriteLine(emp4);
+
